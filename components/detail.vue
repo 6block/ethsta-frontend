4bb3ddbda05706b7b94f27d546cac4b5058f83f3
Xingrui Ni <315631006@qq.com>
2022-03-23 13:49:17 +0800

    FIX:table width

--- components/detail.vue
+++ components/detail.vue
@@ -13,38 +13,38 @@
             <td class="text-center font-bold w-48 text-sm">
               {{ $t('detail.name') }}
             </td>
-            <td class="text-center font-bold w-24 text-sm">
+            <td class="text-center font-bold w-20 text-sm">
               {{ $t('detail.share') }}
             </td>
             <td class="text-center font-bold w-24 text-sm">
-              {{ $t('detail.totalDeposit') }}
+              {{ $t('detail.totalDeposit') }}(ETH)
             </td>
-            <td class="text-center font-bold w-20 text-sm">
-              {{ $t('detail.deposit7d') }}
+            <td class="text-center font-bold w-28 text-sm">
+              {{ $t('detail.deposit7d') }}(ETH)
             </td>
-            <td class="text-center font-bold w-20 text-sm">
+            <td class="text-center font-bold w-24 text-sm">
               {{ $t('detail.totalValidators') }}
             </td>
             <td class="text-center font-bold w-28 text-sm">
               {{ $t('detail.validators7d') }}
             </td>
-            <td class="text-center font-bold w-32 text-sm">
+            <td class="text-center font-bold w-28 text-sm">
               {{ $t('detail.eth1AddrNum') }}
             </td>
-            <td class="text-center font-bold w-32 text-sm">
+            <td class="text-center font-bold w-28 text-sm">
               {{ $t('detail.eth2AddrNum') }}
             </td>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, index) in list" :key="index" class="hover:bg-gray-200 border-b border-gray-200">
-            <td class="text-center w-8 text-xs">
+            <td class="text-center text-xs">
               {{ index + 1 }}
             </td>
-            <td class="text-center w-32 text-xs">
+            <td class="text-center text-xs">
               {{ item.name }}
             </td>
-            <td class="text-center w-24 flex items-center justify-start text-xs">
+            <td class="text-center flex items-center justify-start text-xs">
               <el-popover
                 placement="right"
                 width="200"
@@ -53,24 +53,24 @@
               >
                 <img slot="reference" src="@/assets/img/share/info.png" class="w-5 h-5 cursor-pointer my-2 mr-2">
               </el-popover>
-              {{ item.share }}
+              {{ item.share.toFixed(3) }}%
             </td>
-            <td class="text-center w-28 text-xs">
+            <td class="text-center text-xs">
               {{ item.total_deposit }}
             </td>
-            <td class="text-center w-28 text-xs">
+            <td class="text-center text-xs">
               {{ item.deposit_7d }}
             </td>
-            <td class="text-center w-28 text-xs">
+            <td class="text-center text-xs">
               {{ item.total_validators }}
             </td>
-            <td class="text-center w-28 text-xs">
+            <td class="text-center text-xs">
               {{ item.validators_7d }}
             </td>
-            <td class="text-center w-32 text-xs underline font-bold text-primary-400 cursor-pointer">
+            <td class="text-center text-xs underline font-bold text-primary-400 cursor-pointer">
               <span @click="download(item, 1)">{{ item.eth1_addr_number }}</span>
             </td>
-            <td class="text-center w-32 text-xs underline font-bold text-primary-400 cursor-pointer">
+            <td class="text-center text-xs underline font-bold text-primary-400 cursor-pointer">
               <span @click="download(item, 2)">{{ item.eth2_addr_number }}</span>
             </td>
           </tr>
